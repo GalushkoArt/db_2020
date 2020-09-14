e6baf5e8212e5cfa95ae2a384e649f7c0a107ae3
Artyom <MTRight@ya.ru>
2020-09-14 10:49:27 +0300

    listutils

--- src/main/java/java_8_examples/ListUtils.java
+++ src/main/java/java_8_examples/ListUtils.java
@@ -1,15 +1,16 @@
 package java_8_examples;
 
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * @author Evgeny Borisov
  */
 public class ListUtils {
-    public static <T> int countDuplicates(List<T> list, T t) {
+    public static <T, R> int countDuplicates(List<T> list, T t, Function<T, R> equaler) {
         int counter=0;
         for (T t1 : list) {
-            if (t1.equals(t)) {
+            if (equaler.apply(t1).equals(equaler.apply(t))) {
                 counter++;
             }
         }
