817477bfcbae743665b5090adec8041f52e54c0c
Artyom <MTRight@ya.ru>
2020-09-14 11:05:12 +0300

    listutils

--- src/main/java/java_8_examples/ListUtils.java
+++ src/main/java/java_8_examples/ListUtils.java
@@ -1,16 +1,17 @@
 package java_8_examples;
 
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 /**
  * @author Evgeny Borisov
  */
 public class ListUtils {
-    public static <T, R> int countDuplicates(List<T> list, T t, Function<T, R> equaler) {
+    public static <T, R> int countDuplicates(List<T> list, T t, BiFunction<T, T, Boolean> equaler) {
         int counter=0;
         for (T t1 : list) {
-            if (equaler.apply(t1).equals(equaler.apply(t))) {
+            if (equaler.apply(t1,t)) {
                 counter++;
             }
         }
